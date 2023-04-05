81156151adc489d31760e1417e5be8a6b1f7d269
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-04-05 23:36:40 +0200

    queryInput

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
-
+//--
+var debounce = require('lodash.debounce');
+//--
 const DEBOUNCE_DELAY = 300;
+const ref = {
+  inputEl: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('country-info'),
+};
+
+ref.inputEl.addEventListener('input', debounce(inputListen, DEBOUNCE_DELAY));
+
+function inputListen(e) {
+  console.log(e.target.value.trim());
+}
